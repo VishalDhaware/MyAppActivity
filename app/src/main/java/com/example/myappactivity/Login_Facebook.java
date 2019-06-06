726848bdb9a81f605708e07c17ab6cb217ed5342
Vishal_Dhaware <dhaware090@gmail.com>
2019-06-06 15:32:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myappactivity/Login_Facebook.java
@@ -0,0 +1,13 @@
+package com.example.myappactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Facebook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__facebook);
+    }
+}
